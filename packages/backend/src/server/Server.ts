da81a0a0d65cb283c7581e667dae58c2923d19d9
Rees <reesmorriscontact@gmail.com>
2023-07-24 19:13:26 +0100

    Add some comments and error checking

--- packages/backend/src/server/Server.ts
+++ packages/backend/src/server/Server.ts
@@ -5,6 +5,9 @@ interface ServerStartOptions {
   port?: number;
 }
 
+/**
+ * This class encapsulates the Socket.IO server
+ */
 export class Server {
   private io: IOServer;
 
@@ -12,18 +15,30 @@ export class Server {
     this.io = new IOServer();
   }
 
+  /**
+   * Start the Socket.IO server
+   */
   public start(options: ServerStartOptions = {}): void {
+    // Start the server
     const port = Number(process.env.PORT) || options.port || 3000;
     this.io.listen(port);
 
+    // Log the server start event
     logger.info(`🚀 Server listening on port ${port}`);
 
+    // Listen for new connections
     this.io.on('connection', socket => {
       logger.debug('A user connected');
 
+      // Handle the disconnect event
       socket.on('disconnect', () => {
         logger.debug('User disconnected');
       });
+    });
+
+    // Listen for server errors
+    this.io.on('error', error => {
+      logger.error(`Server error: ${error}`);
     });
   }
 }
