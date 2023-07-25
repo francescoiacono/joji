67e47deebf8065747549c800db89adefc33b2190
Francesco <sennozeta@gmail.com>
2023-07-25 16:19:24 +0100

    form component clean up

--- packages/frontend/src/components/pages/home/subcomponents/createRoomForm/createRoomForm.tsx
+++ packages/frontend/src/components/pages/home/subcomponents/createRoomForm/createRoomForm.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { RoomEvent } from '@joji/types';
+import { RoomEvent, SocketEvent } from '@joji/types';
 import { useEffect, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
+import { useRouter } from 'next/navigation';
 
 type FormState = {
   error: string;
@@ -19,21 +20,15 @@ const CreateRoomForm = () => {
     data: null
   });
 
+  const router = useRouter();
+
   useEffect(() => {
     if (!socket) {
       initialiseSocket();
     } else {
-      socket?.on(RoomEvent.RoomCreated, data => {
-        console.log('Room Created', data);
-        setFormState({ ...formState, data });
-      });
+      handleListeners();
     }
   }, [socket]);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    socket?.emit(RoomEvent.CreateRoom, { hostName: hostName });
-  };
 
   const initialiseSocket = () => {
     setFormState({ ...formState, loading: true });
@@ -50,17 +45,37 @@ const CreateRoomForm = () => {
     }
   };
 
+  const handleListeners = () => {
+    socket?.on(RoomEvent.RoomCreated, data => {
+      setFormState({ ...formState, error: '', data });
+    });
+
+    socket?.on(SocketEvent.Error, error => {
+      setFormState({ ...formState, error: error.code });
+    });
+  };
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setHostName(e.target.value);
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    socket?.emit(RoomEvent.CreateRoom, { hostName: hostName });
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      <section>Create a room!</section>
+      <section>
+        <h2>Create a room!</h2>
+      </section>
       <section>
         <label>Room Name</label>
-        <input type='text' placeholder='TripleHHH' onChange={handleChange} />
+        <input type='text' placeholder='Your name!' onChange={handleChange} />
       </section>
+
+      <section>{formState.error}</section>
+
       <button>Submit</button>
     </form>
   );
