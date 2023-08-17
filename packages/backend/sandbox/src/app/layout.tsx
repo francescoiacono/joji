931da1f5ef2b122e7cf916bd221f6ef39bb4811a
Rees <reesmorriscontact@gmail.com>
2023-08-17 17:44:19 +0100

    Add an [empty] sandbox server to the backend for better testing

---
+++ packages/backend/sandbox/src/app/layout.tsx
@@ -0,0 +1,13 @@
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = (props: LayoutProps) => {
+  return (
+    <html lang='en'>
+      <body>{props.children}</body>
+    </html>
+  );
+};
+
+export default Layout;
